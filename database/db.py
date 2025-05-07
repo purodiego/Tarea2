de132ed6811fe29be93331c858fe9c53067d9f0b
purodiego <diego.zamorano.m@ug.uchile.cl>
2025-05-06 23:25:48 -0400

    tarea2

---
+++ database/db.py
@@ -0,0 +1,21 @@
+import pymysql
+
+DB_NAME = "tarea2"
+DB_USERNAME = "root"
+DB_PASSWORD = "Manzana12345"
+DB_HOST = "localhost"
+DB_PORT = 3306
+DB_CHARSET = "utf8"
+
+def get_conn():
+	conn = pymysql.connect(
+		db=DB_NAME,
+		user=DB_USERNAME,
+		passwd=DB_PASSWORD,
+		host=DB_HOST,
+		port=DB_PORT,
+		charset=DB_CHARSET
+	)
+	return conn
+
+
